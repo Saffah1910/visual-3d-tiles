b6fc7c262f7a1e03ac815c553acac8021abdf0c6
sakhaticpt024 <sakhaticpt024@student.wethinkcode.co.za>
2026-03-27 16:31:45 +0200

    added pillow to venv

--- store/models.py
+++ store/models.py
@@ -58,31 +58,16 @@ class Order(models.Model):
     def __str__(self):
         return f"Order #{self.id} - {self.customer_name}"
 
-
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product_name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.IntegerField()  
 
-# class OrderItem(models.Model):
-#     order = models.ForeignKey(Order, on_delete=models.CASCADE)
-#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-#     quantity = models.IntegerField()
-#     price = models.DecimalField(max_digits=8, decimal_places=2)
-
-
-# class OrderItem(models.Model):
-#     order = models.ForeignKey(
-#         Order,
-#         on_delete=models.CASCADE,
-#         related_name='items'
-#     )
-#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-#     price = models.DecimalField(max_digits=8, decimal_places=2)
-#     quantity = models.PositiveIntegerField()
-
     def __str__(self):
-        return self.product.name
+        return self.product_name
+
+
+
 
 
